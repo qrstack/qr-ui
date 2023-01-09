7a9c2ea8700c7a778fc9cb430d37bcbca8451c07
prd-tan-le <tan.le@paradox.ai>
2023-01-09 21:03:57 +0700

    feat: deploy source code

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -xe
 
-# Delete the old  directory as needed.
+# Delete the old directory as needed.
 if [ -d /usr/local/codedeployresources ]; then
     rm -rf /usr/local/codedeployresources/
 fi
 
 mkdir -vp /usr/local/codedeployresources
+
+if [ -d /home/ec2-user/work/qrstack/qr-ui ]; then
+    rm -rf /home/ec2-user/work/qrstack/qr-ui/
+fi
+
+mkdir -vp /home/ec2-user/work/qrstack/qr-ui
