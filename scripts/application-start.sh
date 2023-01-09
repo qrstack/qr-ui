5ac2fd92975130f4b4a5e851e8ab07968b7c7e6c
prd-tan-le <tan.le@paradox.ai>
2023-01-09 22:49:50 +0700

    fix: codeploy-agent

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -4,6 +4,6 @@ set -xe
 export BASE_DIR=/home/ec2-user/work/qrstack/qr-ui
 
 cd $BASE_DIR
-yarn install && yarn build
-pm2 start yarn --name qr-ui -- start
-sudo service nginx reload
+#yarn install && yarn build
+#pm2 start yarn --name qr-ui -- start
+#sudo service nginx reload
